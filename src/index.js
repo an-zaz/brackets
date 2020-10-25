4239789bc03f2d4a7f3fea1148d7601575c47e5d
Nastya <an_zazerskaya@mail.ru>
2020-10-25 21:03:00 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let open = bracketsConfig.map(item => item[0]);
+    let close = bracketsConfig.map(item => item[1]);
+    let arr = str.split('');
+    let stack = [];
+
+    for (let curr of arr) {
+        let bracketIndex = close.findIndex(item => curr === item );
+        if (bracketIndex !== -1) {
+            if (open[bracketIndex] === close[bracketIndex]) {
+                if (open[bracketIndex] === stack[stack.length - 1]) {
+                    stack.pop();
+                } else {
+                    stack.push(curr);
+                }
+                continue;
+            }
+            if (stack[stack.length - 1] !== open[bracketIndex]) {
+                return false;
+            }
+            stack.pop();
+            continue;
+        }
+        stack.push(curr);
+    }
+    return stack.length === 0;
+};
+
